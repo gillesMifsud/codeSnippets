77b1bc9348aae555e410dd2b7ce14f4236a6faba
Gilles Mifsud <gil.mifsud@gmail.com>
2018-12-26 20:37:39 +0100

    feat: cke editor snippet form

--- src/Form/SnippetType.php
+++ src/Form/SnippetType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Language;
 use App\Entity\Snippet;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -16,7 +17,7 @@ class SnippetType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('content')
+            ->add('content', CKEditorType::class)
             ->add('language', EntityType::class, [
                 'class' => Language::class,
                 'choice_label' => 'name',
